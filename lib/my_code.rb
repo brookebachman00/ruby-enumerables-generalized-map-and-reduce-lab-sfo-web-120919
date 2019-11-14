fb7a7ed3a17c551a01f4de8c566f2b12ad48115c
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 18:05:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,5 +9,14 @@ end
 
 def reduce(array)
   array.each do |item|
-    
+
+end
+def reduce_to_total(source_array, starting_point=0)
+  empty = []
+  array.each do |item|
+    empty << yield(item)
+      return false
+    end
+  end
+  return empty
 end
